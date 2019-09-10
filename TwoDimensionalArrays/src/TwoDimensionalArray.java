ffc94f0b5a27f3e51dd8c290b3cce179319a3dcc
michael.Nguyen <michael.Nguyen@MU-C1-18.mullen.pvt.k12.co.us>
2019-09-10 11:46:22 -0600

    TwoDimmensional

---
+++ TwoDimensionalArrays/src/TwoDimensionalArray.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class TwoDimensionalArray
+	{
+
+		public static void main(String[] args)
+			{
+				// TODO Auto-generated method stub
+
+				int[][] myArray =
+							{
+							{3, 5, 9, 2, -1, 8, 13}, 
+							{-2, 5, 1, 11, 8, 2, 0}, 
+							{9, 1, -3, 4, 8, 4, 8}, 
+							{6, 0, 0, -1, 4, 12, 7}, 
+							{2, 6, -1, 5, 3, 10, 1}, 
+							{2, 5, 1, 2, 9, 2, 3}, 
+							{-3, 1, 1, 4, 11, 12, 4}
+							};
+
+				myArray[0][0] = 3;
+				
+				
+			}
+
+	}
